102ffb7e43585a33ce5f3bc2021e70e597dd1212
kasettakorn <kasettakorn_techno@hotmail.com>
2020-04-08 23:58:18 +0700

    Updates

---
+++ precache-manifest.06a00ab90992334726d80212d0d4f68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a6ecd93ccc20be5bef972f0b3b4aea",
+    "url": "/CIS_KMUTNB-Bingo/index.html"
+  },
+  {
+    "revision": "a6446107b8d93a4b4ecf",
+    "url": "/CIS_KMUTNB-Bingo/static/css/main.8a50b8b7.chunk.css"
+  },
+  {
+    "revision": "b6f3a1dd2ea2487b17a7",
+    "url": "/CIS_KMUTNB-Bingo/static/js/2.18e2ddff.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/CIS_KMUTNB-Bingo/static/js/2.18e2ddff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6446107b8d93a4b4ecf",
+    "url": "/CIS_KMUTNB-Bingo/static/js/main.0a23e1b8.chunk.js"
+  },
+  {
+    "revision": "40f92e89525cc43b906b",
+    "url": "/CIS_KMUTNB-Bingo/static/js/runtime-main.f79a48ab.js"
+  },
+  {
+    "revision": "8bd8d52f7221a4473f7e03a4e75e72ae",
+    "url": "/CIS_KMUTNB-Bingo/static/media/red-1618916_1280.8bd8d52f.png"
+  }
+]);
